9e8c81a94135abae8e87a549f726eeaffc991e56
Andreyrvs <cortaesai@gmail.com>
2022-01-03 10:33:19 -0300

    Css

--- src/components/SolarSystem.jsx
+++ src/components/SolarSystem.jsx
@@ -6,8 +6,7 @@ import PlanetCard from './PlanetCard';
 class SolarSystem extends React.Component {
   render() {
     return (
-      <>
-        <div data-testid="solar-system" />
+      <div data-testid="solar-system">
         <Title headline="Planetas" />
         <div className="planet-container">
           {planets.map((planeta, index) => (
@@ -18,7 +17,7 @@ class SolarSystem extends React.Component {
             />
           ))}
         </div>
-      </>
+      </div>
     );
   }
 }
